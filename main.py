425be1e42f62258dc7de9a7704b43ac0ab24332d
Szmydki, Katarzyna <Katarzyna.Szmydki@pmi.com>
2022-05-19 13:19:13 +0200

    commit

--- main.py
+++ main.py
@@ -98,10 +98,3 @@ countdown(TIME)
 window.mainloop()
 
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
-
-
